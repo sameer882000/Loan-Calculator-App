82269f88785d66644698d45199398f11faa0667a
Sameer <sameer.882000@gmail.com>
2020-10-08 10:39:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function computeLoan(){
 	var interest = (amount * (interest_rate * .01)) / months;
 	var payment = ((amount / months) + interest).toFixed(2);
 	payment = payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-	document.getElementById('payment').innerHTML = "Monthly Payment = $"+payment;
+	document.getElementById('payment').innerHTML = "Monthly Payment = ₹"+payment;
 }
